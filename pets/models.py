9ecb722e79b07e0894b386d909f2f50e0e99a865
Moshi Musukwa <musukwamoshi@gmail.com>
2018-09-20 03:47:53 +0200

    modified layout

--- pets/models.py
+++ pets/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Pets(models.Model):
     #django orm and migrations takes care of the database side of things
     
-    petname=models.CharField(max_length=20)
+    petname = models.CharField(max_length=20)
     poster=models.CharField(max_length=100)
     email=models.CharField(max_length=254)
     phone_number=models.CharField(max_length=12)
@@ -12,7 +12,7 @@ class Pets(models.Model):
     town=models.CharField(max_length=30)
     province=models.CharField(max_length=30)
     description=models.TextField()
-    filename=models.FileField(upload_to='media/')
+    filename=models.FileField(upload_to='media')
     posted_at=models.DateTimeField(default=datetime.now(),blank=False)
 
     def __str__(self):
